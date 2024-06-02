354fa4becf894ac205cb48289f74accc25a99d08
Julito Rojas <jrojasg1@eafit.edu.co>
2024-06-01 19:58:53 -0500

    fix

--- reto2.py
+++ reto2.py
@@ -7,12 +7,8 @@ class StockAnalysis(MRJob):
         return [
             MRStep(mapper=self.mapper_collect_data,
                    reducer=self.reducer_collect_data),
-            MRStep(mapper=self.mapper_find_extremes,
-                   reducer=self.reducer_find_extremes),
-            MRStep(mapper=self.mapper_stable_stocks,
-                   reducer=self.reducer_stable_stocks),
-            MRStep(mapper=self.mapper_black_day,
-                   reducer=self.reducer_black_day)
+            MRStep(reducer=self.reducer_find_extremes_and_stable),
+            MRStep(reducer=self.reducer_black_day)
         ]
 
     def mapper_collect_data(self, _, line):
@@ -35,57 +31,34 @@ class StockAnalysis(MRJob):
         max_price = max(prices, key=lambda x: x[0])
         is_stable = all(prices[i][0] <= prices[i + 1][0] for i in range(len(prices) - 1))
 
-        yield company, (min_price, max_price, is_stable, prices)
-
-    def mapper_find_extremes(self, company, data):
-        min_price, max_price, is_stable, prices = data
-        yield company, (min_price, max_price)
+        yield 'extremes', (company, min_price, max_price)
         yield 'stable_check', (company, is_stable)
+        for date, price in prices:
+            yield date, (company, price)
 
-    def reducer_find_extremes(self, key, values):
-        if key == 'stable_check':
-            for company, is_stable in values:
-                if is_stable:
-                    yield 'stable', company
-        else:
-            yield key, list(values)
-
-    def mapper_stable_stocks(self, key, value):
-        if key == 'stable':
-            yield 'stable', value
-        else:
-            for company, price in value:
-                yield price[1], (company, price[0])  # Emitir (fecha, (empresa, precio))
-
-    def reducer_stable_stocks(self, key, values):
-        if key == 'stable':
-            stable_companies = list(values)
-            yield 'stable', stable_companies
+    def reducer_find_extremes_and_stable(self, key, values):
+        if key == 'extremes':
+            for value in values:
+                yield 'extremes_result', value
+        elif key == 'stable_check':
+            stable_companies = [company for company, is_stable in values if is_stable]
+            yield 'stable_companies', stable_companies
         else:
             prices = list(values)
             min_price = min(prices, key=lambda x: x[1])
             count_min = sum(1 for company, price in prices if price == min_price[1])
-            yield key, count_min
-
-    def mapper_black_day(self, key, value):
-        if key != 'stable':
-            yield 'black_day', (key, value)
+            yield 'black_day_candidate', (key, count_min)
 
     def reducer_black_day(self, key, values):
-        day_counts = {}
-        for date, count in values:
-            if date not in day_counts:
-                day_counts[date] = 0
-            day_counts[date] += count
+        if key == 'black_day_candidate':
+            day_counts = {}
+            for date, count in values:
+                if date not in day_counts:
+                    day_counts[date] = 0
+                day_counts[date] += count
 
-        black_day = max(day_counts, key=day_counts.get)
-        yield 'black_day', black_day
-
-    def reducer_final_output(self, key, values):
-        if key == 'black_day':
-            yield 'black_day', list(values)[0]
-        elif key == 'stable':
-            yield 'stable_companies', list(values)[0]
+            black_day = max(day_counts, key=day_counts.get)
+            yield 'black_day', black_day
         else:
             for value in values:
                 yield key, value

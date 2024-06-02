58f27d04df341bc7b168db35cfd3de5c27dacf9b
Julito Rojas <jrojasg1@eafit.edu.co>
2024-06-01 19:36:27 -0500

    omitir encabezado

--- reto1.py
+++ reto1.py
@@ -14,38 +14,49 @@ class SalaryStatistics(MRJob):
     def mapper_salary(self, _, line):
         # Dividir la línea en campos
         fields = line.strip().split(',')
-        emp_id, se_id, salary = fields[0], fields[1], float(fields[2])
+        if fields[0] == 'idemp':
+            # Ignorar la línea de encabezado
+            return
 
-        # Emitir para calcular el salario promedio por SE
-        yield ('SE', se_id), (salary, 1)
+        try:
+            emp_id, se_id, salary, year = fields
+            salary = float(salary)
 
-        # Emitir para calcular el salario promedio por Empleado
-        yield ('EMP', emp_id), (salary, 1)
+            # Emitir para calcular el salario promedio por SE
+            yield ('SE', se_id), (salary, 1)
+
+            # Emitir para calcular el salario promedio por Empleado
+            yield ('EMP', emp_id), (salary, 1)
+
+            # Emitir para contar el número de SE por Empleado
+            yield (emp_id, 'SE'), se_id
+        except ValueError:
+            # Manejar líneas con datos incorrectos
+            pass
 
     def reducer_salary(self, key, values):
-        total_salary = 0
-        total_count = 0
-
-        for salary, count in values:
-            total_salary += salary
-            total_count += count
-
-        yield key, total_salary / total_count
+        if key[0] in ('SE', 'EMP'):
+            total_salary = 0
+            total_count = 0
+            for salary, count in values:
+                total_salary += salary
+                total_count += count
+            yield key, total_salary / total_count
+        else:
+            sectors = set(values)
+            yield key, len(sectors)
 
     def mapper_sector(self, key, value):
         if key[0] == 'EMP':
-            yield key[1], value
+            yield key[1], ("Average Salary", value)
         elif key[0] == 'SE':
-            yield key[1], (value, 1)
+            yield key[1], ("Average Salary", value)
+        else:
+            yield key, ("Number of SE", value)
 
     def reducer_sector(self, key, values):
-        if isinstance(next(values), float):  # For salary average per employee
-            yield key, ("Average Salary", next(values))
-        else:
-            sectors = set()
-            for sector, count in values:
-                sectors.add(sector)
-            yield key, ("Number of SE", len(sectors))
+        for value_type, value in values:
+            yield key, (value_type, value)
 
 if __name__ == '__main__':
     SalaryStatistics.run()
